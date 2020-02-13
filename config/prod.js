de08cd3d2e52a24d31c4900d8cfae17795852c02
Dev-hoon <asdnm5030@naver.com>
2020-02-14 01:18:48 +0900

    라이브와 테스트 디비 나누기

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONG_URI
+}
--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express()
 const port = 5000
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const { User } = require("./models/User")
+const { User } = require('./models/User')
+const config = require('./config/key')
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 //application.json
-mongoose.connect('mongodb+srv://db_admin:ahdrhelqlqlalfqjsgh@tube-v5mjb.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true,
     useUnifiedTopology:true,
     useCreateIndex:true,
