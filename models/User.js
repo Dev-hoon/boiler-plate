d009565eda4f85dfe8bd85646af86ce7ceee9c09
Dev-hoon <asdnm5030@naver.com>
2020-02-17 13:35:57 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10 // 암호화 자리 숫자
+
 
 const userSchema = mongoose.Schema({
     name:{
@@ -26,6 +29,25 @@ const userSchema = mongoose.Schema({
     token: String,
     tokenExp : Number
 })
+// save 전 동작 임
+userSchema.pre('save',function(next){
+    var user = this;
+    if(user.isModified('password')){
+        // 비밀번호가 수정 될때만 암호화 됨
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    }else{
+        next()
+    }
+})
+
 
 const User = mongoose.model('User', userSchema)
 
